90a305d1b5df1a982eba937c5b1107ee5a6a8ef5
Tiger50 <theo.melaert28@gmail.com>
2026-07-24 16:17:16 +0200

    installing all packages

---
+++ Timetable/src/services/supabaseClient.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+// Création du client unique pour toute l'application
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
